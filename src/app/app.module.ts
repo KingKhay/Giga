c1b3743ae9a71ddd415049b7d9152a85a0f0425e
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-29 10:58:38 +0000

    finished desktop hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeroComponent } from './home/hero/hero.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { ButtonComponent } from './shared/button/button.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './header/navbar/navbar.component';
     HomeComponent,
     HeroComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
