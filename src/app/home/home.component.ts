52f2e7485668ee12947f7796db679a7a06a72d85
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-30 11:13:42 +0000

    added the info sections to the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Info, INFO_DATA} from "./info/model/Info";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  infoData: Info[] = INFO_DATA;
 }
