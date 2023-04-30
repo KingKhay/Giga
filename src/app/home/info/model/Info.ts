9b76892016574662032cd6ac4e4c4eea5df9c5c1
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-30 11:11:06 +0000

    created a model for the info component

--- src/app/home/info/model/Info.ts
+++ src/app/home/info/model/Info.ts
@@ -1,13 +1,24 @@
-[{
+export interface Info{
+  title: string;
+  description: string;
+  svgUrl: string;
+  hasButton: boolean;
+}
+
+
+export const INFO_DATA: Info[] = [{
   "title": "Light, Fast & Powerful",
   "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus \nmus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
-  "svgUrl" : "assets/images/info1.png"
+  "svgUrl" : "assets/images/info1.png",
+  "hasButton": false
 },{
   "title": "Light, Fast & Powerful",
   "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus \nmus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
-  "svgUrl" : "assets/images/info2.png"
+  "svgUrl" : "assets/images/info2.png",
+  "hasButton": false
 },{
   "title": "Light, Fast & Powerful",
   "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus \nmus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
-  "svgUrl" : "assets/images/info3.png"
-}]
+  "svgUrl" : "assets/images/info3.png",
+  "hasButton": true
+}];
