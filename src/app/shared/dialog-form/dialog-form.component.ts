ed1975f2d5bc6551c1a72e23f2cbbe838255270f
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-06 12:57:12 +0000

    created the dialog form component

---
+++ src/app/shared/dialog-form/dialog-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-form',
+  templateUrl: './dialog-form.component.html',
+  styleUrls: ['./dialog-form.component.css']
+})
+export class DialogFormComponent {
+
+}
