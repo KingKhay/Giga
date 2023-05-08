ba9b0fbfb965dfd6595cd9107779c5db164b099d
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-08 15:05:43 +0000

    created the basic reactive form structure in the dialog form component

--- src/app/shared/dialog-form/dialog-form.component.ts
+++ src/app/shared/dialog-form/dialog-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-dialog-form',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-form.component.css']
 })
 export class DialogFormComponent {
+  constructor(private fb: FormBuilder) {
 
+  }
+
+  myForm: FormGroup = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+  })
 }
